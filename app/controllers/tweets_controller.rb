b8bdef88e5fbbd4a727ccb66deb23bff218e6c81
Khushal Dave <khushaldave@gmail.com>
2014-04-04 22:49:51 +0100

    Added Flash and Striped_Tags for Access Devices

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
 
   def create
     current_user.tweet(twitter_params[:message])
+ 
   end
 
   def twitter_params
@@ -27,24 +28,29 @@ class TweetsController < ApplicationController
 
   def search
     @tweets=current_user.search(tweet_params[:hashtag])
-    @tweet=[]
-    @tweets.each do |tweet|
-     if(tweet.user.location.present?)
-    @tweet<<
-    {
-    :location => tweet.user.location,
-    :text => tweet.text,
-    :profile_pic => tweet.user["profile_image_url"],
-    :status_count => tweet.user.statuses_count,
-    :friends_count => tweet.user.friends_count,
-    :lat => Geocoder.search(tweet.user.location.to_s).first.coordinates.first,
-    :lng => Geocoder.search(tweet.user.location.to_s).first.coordinates.last
-     }
-   else
+    if @tweet.nil?
+      flash.now[:notice] = "Soory no tweets Found !"
+       render "search"
+     end
+      @tweet=[]
+      @tweets.each do |tweet|
+      if(tweet.user.location.present?)
+          source=tweet.source.dup
+          @tweet<<
+            {
+              :location => tweet.user.location,
+              :text => tweet.text,
+              :profile_pic => tweet.user["profile_image_url"],
+              :status_count => tweet.user.statuses_count,
+              :friends_count => tweet.user.friends_count,
+              :lat => Geocoder.search(tweet.user.location.to_s).first.coordinates.first,
+              :lng => Geocoder.search(tweet.user.location.to_s).first.coordinates.last,
+              :Source => ActionController::Base.helpers.strip_tags(source)
+            }
+      else
+      end
+    end
    end
-   end
-
-  end
 
 
   def tweet_params
